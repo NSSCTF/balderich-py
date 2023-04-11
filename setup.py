4a78ff66306c21e8ced4fa0e790fc8e848e428b9
X3NNY <xenny@vip.qq.com>
2023-04-11 17:00:42 +0800

    修改setup配置

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
-import setuptools
-setuptools.setup()
+from os import path as os_path
+from setuptools import setup
+
+this_directory = os_path.abspath(os_path.dirname(__file__))
+
+def read_file(filename):
+    with open(os_path.join(this_directory, filename), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
+setup(
+    long_description=read_file('README.md')
+)
