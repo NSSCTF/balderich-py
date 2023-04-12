c307bb35a50c08e4eee6131556eb7df327c66cb8
X3NNY <xenny@vip.qq.com>
2023-04-12 13:41:56 +0800

    fix circle import

--- src/balderich/models/resource.py
+++ src/balderich/models/resource.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
-from balderich.client import NSSClient
 
 class Collection:
     """
     A base class fro representing all objects of a particular type on the server.
     """
 
-    def __init__(self, client: NSSClient=None):
+    def __init__(self, client = None):
         self.client = client
